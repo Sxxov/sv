849470a3fc37780265f724604b468ea3624b8a80
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-07-10 15:02:16 +0800

    Refactor button variants to be composable

--- src/lib/button/ButtonVariants.ts
+++ src/lib/button/ButtonVariants.ts
@@ -1,52 +1,32 @@
 import type Button from './Button.svelte';
 
-const Primary = {
-	colourBackground: '----colour-accent-tertiary',
-	colourBackgroundHover: '----colour-accent-secondary',
-	colourText: '----colour-accent-primary',
-	colourTextHover: '----colour-accent-primary',
-} satisfies Button['$$prop_def'];
-
-const Secondary = {
-	colourBackground: '----colour-background-secondary',
-	colourBackgroundHover: '----colour-background-tertiary',
-	colourText: '----colour-text-primary',
-	colourTextHover: '----colour-text-primary',
-} satisfies Button['$$prop_def'];
-
-const FabRegularPrimary = {
-	...Primary,
-	width: '56px',
-	height: '56px',
-	padding: '16px',
-} satisfies Button['$$prop_def'];
-
-const FabRegularSecondary = {
-	...Secondary,
-	width: '56px',
-	height: '56px',
-	padding: '16px',
-} satisfies Button['$$prop_def'];
-
-const FabSmallPrimary = {
-	...Primary,
-	width: '28px',
-	height: '28px',
-	padding: '2px',
-} satisfies Button['$$prop_def'];
-
-const FabSmallSecondary = {
-	...Secondary,
-	width: '28px',
-	height: '28px',
-	padding: '2px',
-} satisfies Button['$$prop_def'];
+interface ILeaf<T> {
+	[n: string]: T | ILeaf<T>;
+}
 
 export const ButtonVariants = {
-	Primary,
-	Secondary,
-	FabRegularPrimary,
-	FabRegularSecondary,
-	FabSmallPrimary,
-	FabSmallSecondary,
-} as const;
+	Primary: {
+		colourBackground: '----colour-accent-tertiary',
+		colourBackgroundHover: '----colour-accent-secondary',
+		colourText: '----colour-accent-primary',
+		colourTextHover: '----colour-accent-primary',
+	},
+	Secondary: {
+		colourBackground: '----colour-background-secondary',
+		colourBackgroundHover: '----colour-background-tertiary',
+		colourText: '----colour-text-primary',
+		colourTextHover: '----colour-text-primary',
+	},
+	Fab: {
+		Regular: {
+			width: '56px',
+			height: '56px',
+			padding: '16px',
+		},
+		Small: {
+			width: '28px',
+			height: '28px',
+			padding: '2px',
+		},
+	},
+} as const satisfies ILeaf<Button['$$prop_def']>;
