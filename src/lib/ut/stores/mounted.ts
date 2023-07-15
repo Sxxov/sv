50665533dfdccd0f95941151e72f86037b7c3553
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-07-15 19:19:47 +0800

    Add `mounted`

---
+++ src/lib/ut/stores/mounted.ts
@@ -0,0 +1,72 @@
+import { Store } from '@sxxov/ut/store';
+import { onMount, getAllContexts } from 'svelte';
+
+class MountedStore extends Store<boolean> {
+	constructor() {
+		super(undefined as never);
+	}
+
+	public override destroy() {}
+
+	public override trigger() {}
+
+	public override get() {
+		try {
+			// the only svelte function that internally accesses
+			// `get_current_component()` without side effects.
+			// will throw if not called during component init,
+			// & thus we're mounted if it does throw.
+			getAllContexts();
+
+			return false;
+		} catch {
+			return true;
+		}
+	}
+
+	public override subscribe(
+		onChanged: (v: boolean) => void,
+		onInvalidate?: (() => void) | undefined,
+	) {
+		let subscribed = true;
+
+		try {
+			onMount(() => {
+				if (subscribed) {
+					onInvalidate?.();
+					onChanged(true);
+				}
+			});
+
+			onChanged(false);
+		} catch {
+			onChanged(true);
+		}
+
+		return () => {
+			subscribed = false;
+		};
+	}
+
+	public override subscribeLazy(
+		onChanged: (v: boolean) => void,
+		onInvalidate?: (() => void) | undefined,
+	) {
+		let subscribed = true;
+
+		try {
+			onMount(() => {
+				if (subscribed) {
+					onInvalidate?.();
+					onChanged(true);
+				}
+			});
+		} catch {}
+
+		return () => {
+			subscribed = false;
+		};
+	}
+}
+
+export const mounted = new MountedStore().supply;
