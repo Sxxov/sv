477e7d4caafeed7654289b3d57f7117759521606
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-09 13:19:23 +0800

    Expose `<Void>`

--- src/lib/functional/index.ts
+++ src/lib/functional/index.ts
@@ -2,3 +2,5 @@ export { default as R } from './R.svelte';
 export * from './R.svelte';
 export { default as Rr } from './Rr.svelte';
 export * from './Rr.svelte';
+export { default as Void } from './Void.svelte';
+export * from './Void.svelte';
