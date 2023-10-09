72ed2013ba18aeec8ec22ddc4ff7037e85883c21
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-09 12:48:34 +0800

    Remove `autoplay`
    * Just use `<Void>{tween.play()}</Void>`

--- src/lib/animation/Composition.svelte
+++ src/lib/animation/Composition.svelte
@@ -6,25 +6,15 @@
 </script>
 
 <script lang="ts">
-	import {
-		type AnimatableIterationCount,
-		Composition,
-		type Timeline,
-	} from '@sxxov/ut/animation';
-	import { onMount, setContext } from 'svelte';
+	import { Composition, type Timeline } from '@sxxov/ut/animation';
+	import { setContext } from 'svelte';
 	import type { CompositionContext } from './CompositionContext';
 
 	export let timeline: Timeline | undefined = undefined;
-	// eslint-disable-next-line @typescript-eslint/ban-types
-	export let autoplay: boolean | AnimatableIterationCount | (number & {}) = 0;
 
 	const composition = new Composition(timeline);
 
 	setContext<CompositionContext>(compositionContextKey, { composition });
-
-	onMount(() => {
-		if (autoplay) void composition.play(1, Number(autoplay));
-	});
 </script>
 
 <slot {composition} />
--- src/lib/animation/Tween.svelte
+++ src/lib/animation/Tween.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import {
-		Tween,
-		type TimelineAt,
-		type AnimatableIterationCount,
-	} from '@sxxov/ut/animation';
+	import { Tween, type TimelineAt } from '@sxxov/ut/animation';
 	import type { ReadableBezier } from '@sxxov/ut/bezier';
-	import { getContext, onDestroy, onMount } from 'svelte';
+	import { getContext, onDestroy } from 'svelte';
 	import { compositionContextKey } from '.';
 	import type { CompositionContext } from './CompositionContext';
 
@@ -14,18 +10,12 @@
 	export let duration: number;
 	export let bezier: ReadableBezier | undefined = undefined;
 	export let at: TimelineAt = undefined;
-	// eslint-disable-next-line @typescript-eslint/ban-types
-	export let autoplay: boolean | AnimatableIterationCount | (number & {}) = 0;
 
 	const tween = new Tween(start, end, duration, bezier);
 	const { composition } = getContext<CompositionContext>(
 		compositionContextKey,
 	);
 	if (composition) composition.add(tween, at);
-
-	onMount(() => {
-		if (autoplay) void tween.play(1, Number(autoplay));
-	});
 
 	onDestroy(() => {
 		composition.remove(tween);
