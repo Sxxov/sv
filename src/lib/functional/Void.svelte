67d75ebf05236a8d387eecb0482c421e7d7e9ded
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-09 12:43:08 +0800

    Add `Void`

---
+++ src/lib/functional/Void.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { createEventDispatcher, onDestroy, onMount } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	onMount(() => {
+		dispatch('in');
+	});
+
+	onDestroy(() => {
+		dispatch('out');
+	});
+</script>
