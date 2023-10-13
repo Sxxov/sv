3cebcf8636b6cdfb20ef3d49a4dc5dccdfd42a40
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-13 23:33:20 +0800

    Respect `delay` transition config

--- src/lib/ut/transition/transitions/sustain.ts
+++ src/lib/ut/transition/transitions/sustain.ts
@@ -1,8 +1,12 @@
+import type { TransitionConfig, TransitionFrame } from '..';
 import type { Transition } from '../Transition';
 
-export const sustain: Transition = () => ({
-	delay: 0,
+export const sustain: Transition = (
+	element: Element,
+	{ delay = 0 }: { delay?: number } = {},
+) => ({
+	delay,
 	duration: 0,
 	easing: (t) => t,
-	css: () => '',
+	css: (t) => `display: ${t > 0 ? '' : 'none'}`,
 });
